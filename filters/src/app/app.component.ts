238dfaeef5cdb04b8a738c8e1d13370d951f4917
Karen Celis <47069950+KarenCelis@users.noreply.github.com>
2020-08-25 23:53:04 -0500

    JSON pipe

--- filters/src/app/app.component.ts
+++ filters/src/app/app.component.ts
@@ -11,4 +11,44 @@ export class AppComponent {
   date = new Date();
   currency = 100;
   month='Month';
+
+
+  /*packages = {
+    titulo: 'Basic',
+    precio: 100,
+    month : 'month',
+    d1: 'socil media',
+    d2: 'branding',
+    d3: 'Image Desing',
+    d4:'-'
+  }*/
+
+  packages = [{
+    title: 'Basic',
+    price: 100,
+    month : 'month',
+    d1: 'social media',
+    d2: 'branding',
+    d3: 'Image Desing',
+    d4:'-'
+  },
+  {
+    title: 'Recommended',
+    price: 500,
+    month : 'month',
+    d1: 'social media',
+    d2: 'branding',
+    d3: 'Image Desing',
+    d4:'Video'
+  },
+  {
+    title: 'Advanced',
+    price: 900,
+    month : 'month',
+    d1: 'social media',
+    d2: 'branding',
+    d3: 'Image/ Video',
+    d4:'Community Manager'
+  }];
+
 }
